d9845720d49cc9c770f84e25b76dbac98014f65a
Elaine Crisostomo <elaine@greenspacehealth.com>
2022-09-30 15:41:58 -0700

    remove stuff to make this work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
-import preact from "@astrojs/preact";
+// import preact from "@astrojs/preact";
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://feature-tour.netlify.app',
   integrations: [
-    preact(),
+    // preact(),
     sitemap({
       canonicalURL: 'https://feature-tour.netlify.app'
     })
