9c4a84b3a445adf1beb9e1e3b710e75b2657c9e9
Elaine Crisostomo <elaine@greenspacehealth.com>
2022-10-07 14:32:19 -0700

    test deployContext

--- netlify/functions/deploy-building.js
+++ netlify/functions/deploy-building.js
@@ -1,6 +1,6 @@
-const fetch = require('node-fetch');
+const fetch = require('cross-fetch');
 
-exports.handler = async function(event, context) {
+exports.handler = async () => {
   const siteName = process.env.SITE_NAME;
   const deployContext = process.env.CONTEXT;
   const slackUserId = process.env.SLACK_WEBHOOK_USER_ID;
@@ -11,30 +11,31 @@ exports.handler = async function(event, context) {
   const webhookUrl = `https://hooks.slack.com/services/${slackUserId}/${slackChannelId}/${slackApiSecret}`;
   const buildLogUrl = `https://app.netlify.com/sites/${siteName}/deploys/${deployId}`;
 
-  console.log(JSON.parse(event.body))
-
   // Only send message during main production deployment
-    await fetch(webhookUrl, {
-      method: 'POST',
-      headers: {
-      'Content-type': 'application/json',
+  if(deployContext == 'production'){
+      await fetch(webhookUrl, {
+        method: 'POST',
+        headers: {
+        'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+        text: `There's a new deploy in process for ${deployContext}`,
+        blocks: [
+            {
+            type: 'section',
+            text: {
+                type: 'mrkdwn',
+                text: `\n>Visit the <${buildLogUrl} |build log>`,
+            },
+            },
+          ],
+        }),
       },
-      body: JSON.stringify({
-      text: `There's a new deploy in process for ${deployContext}`,
-      blocks: [
-          {
-          type: 'section',
-          text: {
-              type: 'mrkdwn',
-              text: `\n>Visit the <${buildLogUrl} |build log>`,
-          },
-          },
-        ],
-      }),
-    },
-  );
+    );
+  }
 
-    return {
-      statusCode: 200
-    };
+
+  return {
+    statusCode: 200
+  };
 };
